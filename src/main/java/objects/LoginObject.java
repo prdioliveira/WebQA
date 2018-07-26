a1d36a81a7ef4a0e236c4e014305c3d262e91a2c
prdioliveira <prdioliveira@gmail.com>
2018-07-25 21:30:36 -0300

    Inclusao do metofdo de login e definicao da pagina inicial da aplicacao

--- src/main/java/objects/LoginObject.java
+++ src/main/java/objects/LoginObject.java
@@ -1,5 +1,6 @@
 package objects;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 /**
@@ -13,5 +14,19 @@ public class LoginObject extends BaseObject {
 
     public LoginObject(WebDriver driver){
         super(driver);
+    }
+
+    /*
+    * Ao realizar o login a pagina inicial sera a dashboard
+    * */
+    public DashBoardObject logar(String user, String pass){
+        fazerLogin(user, pass);
+        return new DashBoardObject(getDriver());
+    }
+
+    public void fazerLogin(String user, String pass){
+        getDriver().findElement(By.id("inputEmail")).sendKeys(user);
+        getDriver().findElement(By.id("inputPassword")).sendKeys(pass);
+        getDriver().findElement(By.id("login")).click();
     }
 }
